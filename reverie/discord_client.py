af9baa0b6fb25c0ace71d8206fa2dae8655ddaa9
Rewind <wasolomon98@gmail.com>
2025-01-15 04:17:16 -0600

    Attempted to add the foundations for Discord functionality

--- reverie/discord_client.py
+++ reverie/discord_client.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
 import discord
 
 from reverie.conversation_manager import initialize_conversation_log, initialize_conversation, append_message
 from reverie.gpt_utils import query_gpt
 
-DISCORD_TOKEN = os.getenv("OPENAI_API_KEY")
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_API_KEY")
 
 intents = discord.Intents.default()
 intents.message_content = True
