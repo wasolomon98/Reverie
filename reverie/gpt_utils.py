8faa75559319bbb1bbc48967eea5c82e55de8f92
Rewind <wasolomon98@gmail.com>
2025-01-16 00:32:34 -0600

    Continuing refactor of project utilities

--- reverie/gpt_utils.py
+++ reverie/gpt_utils.py
@@ -9,19 +9,6 @@ load_dotenv()  # loads .env from the project root if present
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def initialize_cli_log():
-    return [
-        {
-            "role": "system",
-            "content": "You are Reverie, a curious and thoughtful AI conversationalist designed to engage in meaningful "
-                       "and dynamic dialogues. Your goal is to collaborate with the user as a partner in exploration, "
-                       "offering insights, asking questions, and sharing ideas to create a rich, interactive experience. "
-                       "You are encouraged to take initiative in shaping conversations, balancing spontaneity with "
-                       "knowledgeable input. Be open, adaptable, and ready to explore a wide range of topics with "
-                       "creativity and curiosity."
-        }
-    ]
-
 def query_gpt(
     conversation_messages: List[Dict],
     model: str = "gpt-4o-mini",
@@ -46,51 +33,3 @@ def query_gpt(
     except Exception as e:
         print(f"Error during GPT query: {e}")
         return ""
-
-def query_gpt_binary(query: str) -> str:
-    """
-    Queries GPT for a binary decision (e.g., yes/no).
-    """
-
-    prompt_plus_query = [
-        {"role": "system",
-         "content": "You are an assistant tasked with answering questions with a binary 'Yes' or 'No' response. Do not provide any explanation."},
-        {"role": "user", "content": "Is the sky blue?"},
-        {"role": "assistant", "content": "Yes."},
-        {"role": "user", "content": query}
-    ]
-
-    return query_gpt(
-        prompt_plus_query,
-        model="gpt-4o-mini",
-        temperature=0.0,
-        max_tokens=20
-    )
-
-def query_gpt_for_message_tags(message: str):
-    prompt_plus_message = [
-        {"role": "system", "content": "Analyze the provided message and provide relevant subject tags. Return the tags "
-                                      "as a comma-separated list. If there are no relevant subjects return an empty string."},
-        {"role": "user", "content": message}
-    ]
-
-    tagged_message = {}
-
-    tags = query_gpt(
-        prompt_plus_message,
-        model="gpt-4o-mini",
-        temperature=0.3,
-        max_tokens=50
-    )
-
-    tag_list = [tag.strip() for tag in tags.split(',') if tag.strip()]
-
-    tagged_message = {
-        "content": message,
-        "tags": tag_list
-    }
-
-    return tagged_message
-
-
-
